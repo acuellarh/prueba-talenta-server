04aad2aee24db70f40a4347fd03aade61021c77e
Alejandro <acuellarh@gmail.com>
2022-11-23 22:27:42 -0500

    add cors

--- app.ts
+++ app.ts
@@ -1,20 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import infoTextsRoutes from "./routes/infoTexts";
 
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/infoTexts", infoTextsRoutes)
 
 app.get("/ping", (_req, res) => {
   console.log("Hola Mundo otra vez")
   res.send("Hello Alejo")
 })
-
-
-
-
-
-
 
 
 
